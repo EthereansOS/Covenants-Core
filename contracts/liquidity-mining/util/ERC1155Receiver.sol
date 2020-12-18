d32e1ddc937d51fee039002eb98a6b3a8e5f0a67
PaoloRollo <paolorollo1997@hotmail.it>
2020-12-19 00:10:21 +0100

    add missing contracts.

---
+++ contracts/liquidity-mining/util/ERC1155Receiver.sol
@@ -0,0 +1,50 @@
+// File: contracts/usd-v2/util/ERC1155Receiver.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+abstract contract ERC1155Receiver {
+    bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
+
+    mapping(bytes4 => bool) private _supportedInterfaces;
+
+    constructor() {
+        _registerInterface(_INTERFACE_ID_ERC165);
+        _registerInterface(
+            ERC1155Receiver(0).onERC1155Received.selector ^
+            ERC1155Receiver(0).onERC1155BatchReceived.selector
+        );
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view returns (bool) {
+        return _supportedInterfaces[interfaceId];
+    }
+
+    function _registerInterface(bytes4 interfaceId) internal virtual {
+        require(interfaceId != 0xffffffff, "ERC165: invalid interface id");
+        _supportedInterfaces[interfaceId] = true;
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    )
+        external
+        virtual
+        returns(bytes4);
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    )
+        external
+        virtual
+        returns(bytes4);
+}
