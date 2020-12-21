c6ccbaf259d32f3c790eb77eb369fe0d29023fd8
PaoloRollo <paolorollo1997@hotmail.it>
2020-12-22 00:01:47 +0100

    add reward token address.

--- test/liquidityMining.js
+++ test/liquidityMining.js
@@ -3,11 +3,8 @@ const LiquidityMining = artifacts.require("LiquidityMining");
 const UniswapV2AMMV1 = artifacts.require("UniswapV2AMMV1");
 
 const zero = "0x0000000000000000000000000000000000000000";
-// TODO change orchestrator address
 const orchestratorAddress = "0x12329b2F9e52C5D3422D6E6C026AA9D5b00CC075";
-// TODO change reward token address
-const rewardTokenAddress = zero;
-// TODO change uniswap address
+const rewardTokenAddress = "0x2823589Ae095D99bD64dEeA80B4690313e2fB519";
 const uniswapAddress = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
 // TODO change liquidity pool token address
 const liquidityPoolTokenAddress = zero;
