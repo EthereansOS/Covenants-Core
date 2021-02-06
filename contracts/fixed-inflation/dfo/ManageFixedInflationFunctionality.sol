f9636df8b1b423efd1cec2591f3428a295b17ab2
Marco Vasapollo <ceo@metaring.com>
2021-02-06 20:02:26 +0100

    WIP

--- contracts/fixed-inflation/dfo/ManageFixedInflationFunctionality.sol
+++ contracts/fixed-inflation/dfo/ManageFixedInflationFunctionality.sol
@@ -1,3 +1,9 @@
+/* Discussion:
+ * //discord.gg/34we8bh
+ */
+/* Description:
+ * Fixed Inflation Management Functionality
+ */
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 pragma abicoder v2;
